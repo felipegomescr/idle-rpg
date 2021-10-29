7041800f8d2ca1f94310f686ee6b426a327a523c
felipegomescr <felipe.gomes@comprarapida.co>
2021-10-29 16:44:43 -0300

    Reorder trees

--- src/entities/trees.ts
+++ src/entities/trees.ts
@@ -6,28 +6,28 @@ import type { Tree } from "@/models";
 const mahogany: Tree = {
 	id: uid(),
 	name: "Mahogany",
-	requiredExp: 400,
-	rewardedExp: 30 * progressModifier,
-	rewardTable: [birdNest, mahoganyLog],
-	timeUntilReward: 15000 / progressModifier,
-};
-
-const oak: Tree = {
-	id: uid(),
-	name: "Oak",
 	requiredExp: 0,
 	rewardedExp: 10 * progressModifier,
 	rewardTable: [birdNest, oakLog],
 	timeUntilReward: 5000 / progressModifier,
 };
 
-const willow: Tree = {
+const oak: Tree = {
 	id: uid(),
-	name: "Willow",
+	name: "Oak",
 	requiredExp: 200,
 	rewardedExp: 20 * progressModifier,
 	rewardTable: [birdNest, willowLog],
 	timeUntilReward: 10000 / progressModifier,
+};
+
+const willow: Tree = {
+	id: uid(),
+	name: "Willow",
+	requiredExp: 400,
+	rewardedExp: 30 * progressModifier,
+	rewardTable: [birdNest, mahoganyLog],
+	timeUntilReward: 15000 / progressModifier,
 };
 
 const teak: Tree = {
